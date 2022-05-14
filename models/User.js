048e013d85ff50f6c7db0d3f3086dcf6c83ad599
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-13 23:05:44 -0400

    added the User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+
+//schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("user", userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose");
+const db = require("./config/connection");
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
 });
