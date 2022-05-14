90a3b1f276cfc993bdf6052ec871d63e6a1d4067
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-13 22:33:19 -0400

    added express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
